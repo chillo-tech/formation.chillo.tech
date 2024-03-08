889b641f4da0aa8002cf00d952169a4e75548831
Bright Efoo <brightefoo@gmail.com>
2024-03-08 14:10:49 +0100

    fin premiere version

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,14 +1,32 @@
-import { FaArrowRight } from "react-icons/fa6";
-import { CodeView, TextSlideShow } from "..";
 import { HomeHeaderTextsSlides } from "@/data";
+import { useMemo } from "react";
+import { FaArrowRight } from "react-icons/fa6";
+import { TypeAnimation } from "react-type-animation";
+import { CodeView } from "..";
 
 const Hero = () => {
+  const letterTime = 200;
+  const typeData = useMemo(
+    () =>
+      HomeHeaderTextsSlides.map((el) => {
+        const firstValue = `${el.prefix ? el.prefix + " " : ""}${el.value}`;
+        return [firstValue, firstValue.length * letterTime];
+      }).flat(),
+    []
+  );
   return (
     <section className="mt-2 mb-4 py-2">
       <div className="container mx-auto flex gap-9 flex-col md:flex-row md:items-center">
         <div className="max-w-[90%] mx-auto sm:max-w-fit flex flex-col gap-4">
-          <h1 className="text-heading font-extrabold text-5xl my-4 flex flex-wrap items-center gap-2">
-            formez vous <TextSlideShow texts={HomeHeaderTextsSlides} />
+          <h1 className="text-heading font-extrabold text-5xl my-4">
+            formez vous <br />
+            <TypeAnimation
+              sequence={typeData}
+              wrapper="span"
+              speed={15}
+              className="text-5xl"
+              repeat={Infinity}
+            />
           </h1>
           <p className="font-2xl text-heading">
             Trouvez <span className="font-bold">un boulot</span> et devenez{" "}
